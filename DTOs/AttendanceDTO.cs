bf8a5fa304e490aa0ddfdf4c8bf92171a53b1e19
Nisa Dost <i.n.dost38@gmail.com>
2025-05-19 17:10:25 +0300

    fix attendance list and quiz isactive checkbox

--- DTOs/AttendanceDTO.cs
+++ DTOs/AttendanceDTO.cs
@@ -2,9 +2,13 @@
 {
     public class AttendanceDTO
     {
-        public Guid Id { get; set; }
+        public Guid AttendanceId { get; set; }
+        public Guid InstitutionId { get; set; }
         public Guid StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int StudentGrade { get; set; }
         public Guid LessonId { get; set; }
+        public string LessonName { get; set; }
         public DateTime Date { get; set; }
         public bool IsPresent { get; set; }
         public string Emotion { get; set; }
