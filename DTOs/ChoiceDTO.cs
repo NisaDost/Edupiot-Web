a4f758040deff5623863aafac2deb81c04ed3565
Nisa Dost <i.n.dost38@gmail.com>
2025-05-20 18:43:43 +0300

    update quizdetails page - unfunctional

--- DTOs/ChoiceDTO.cs
+++ DTOs/ChoiceDTO.cs
@@ -2,6 +2,7 @@
 {
     public class ChoiceDTO
     {
+        public Guid ChoiceId { get; set; }
         public string ChoiceContent { get; set; }
         public bool IsCorrect { get; set; }
     }
